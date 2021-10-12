8ae257a5f137ffd8cdbb2bc9da0dadb78d3f604f
Eduarda <eduardacenteno@hotmail.com>
2021-10-12 17:58:32 +0200

    updated ephyviewer and added spikes

--- test_ephyviewer.py
+++ test_ephyviewer.py
@@ -7,68 +7,92 @@ from probeinterface import read_spikeglx
 from probeinterface.plotting import plot_probe
 import numpy as np
 
+
+# Paths
 base_folder = Path('/home/arthur/Documents/SpikeSorting/Test_20210518/') 
-
 data_folder = base_folder / 'raw_awake'
+sorting_folder = Path('/media/storage/spikesorting_output/sorting_pipeline_out_29092021_try/')
+sorting_sub_path = 'full/filter+cmr_radius/tridesclous/custom_tdc_1/'
 
 
-recording_spike = si.SpikeGLXRecordingExtractor(data_folder, stream_id='imec0.ap')
-#~ print(recording)
-
-#~ recording_lf = si.SpikeGLXRecordingExtractor(data_folder, stream_id='imec0.lf')
-#~ print(recording)
-
-#~ recording_nidq = si.SpikeGLXRecordingExtractor(data_folder, stream_id='nidq')
-#~ print(recording_nidq)
+# Folders
+folder = sorting_folder / sorting_sub_path
+print(folder)
 
 
-#~ probe = read_spikeglx(data_folder / 'raw_awake_01_g0_t0.imec0.ap.meta')
-#~ print(probe)
-#~ recording = recording.set_probe(probe)
-#~ print(recording)
+# Main recording files
+recording_spike = si.SpikeGLXRecordingExtractor(data_folder, stream_id='imec0.ap') # ap file
+#print(recording)
 
-#~ exit()
+recording_lf = si.SpikeGLXRecordingExtractor(data_folder, stream_id='imec0.lf') # lfp
+#print(recording)
 
+recording_nidq = si.SpikeGLXRecordingExtractor(data_folder, stream_id='nidq') # microphone
+#print(recording_nidq)
+
+
+# Probe
+probe = read_spikeglx(data_folder / 'raw_awake_01_g0_t0.imec0.ap.meta')
+#print(probe)
+recording_spike = recording_spike.set_probe(probe)
+#print(recording)
+#exit()
+
+
+# Sorting
+#sorting = si.SpykingCircusSortingExtractor(folder) # SpykingCircusSortingExtractor
+sorting = si.TridesclousSortingExtractor(folder)    # TridesclousSortingExtractor
+#print(sorting)
+
+
+# Spike source
+spike_source = ephyviewer.FromSpikeinterfaceSorintgSource(sorting)
+
+
+# Filtering
 recording_f = si.bandpass_filter(recording_spike)
 
 
-sig_source0 = ephyviewer.FromSpikeinterfaceRecordingSource(recording=recording_spike)
-sig_source4 = ephyviewer.FromSpikeinterfaceRecordingSource(recording=recording_f)
-#~ sig_source1 = ephyviewer.FromSpikeinterfaceRecordingSource(recording=recording_lf)
-#~ sig_source2 = ephyviewer.FromSpikeinterfaceRecordingSource(recording=recording_nidq)
+# Sources
+sig_source0 = ephyviewer.FromSpikeinterfaceRecordingSource(recording=recording_spike) # original ap
+sig_source1 = ephyviewer.FromSpikeinterfaceRecordingSource(recording=recording_lf) # lfp
+sig_source2 = ephyviewer.FromSpikeinterfaceRecordingSource(recording=recording_nidq) # microphone
+sig_source3 = ephyviewer.FromSpikeinterfaceRecordingSource(recording=recording_f) # filtered ap trace
 
+
+# App and viewer objects
 app = ephyviewer.mkQApp()
 win = ephyviewer.MainViewer(debug=True, show_auto_scale=True)
 
-view0 = ephyviewer.TraceViewer(source=sig_source0, name='signals')
+view0 = ephyviewer.TraceViewer(source=sig_source0, name='signals') # Trace of ap signal
 win.add_view(view0)
 
-view5 = ephyviewer.TraceViewer(source=sig_source4, name='signals F')
-win.add_view(view5 )
+view1 = ephyviewer.TraceViewer(source=sig_source1, name='signals lf') # Trace of LFP
+win.add_view(view1)
+
+view1_tf = ephyviewer.TimeFreqViewer(source=sig_source1, name='timefreq') # Timefreq view of LFP
+win.add_view(view1_tf)
+
+view2 = ephyviewer.TraceViewer(source=sig_source2, name='signals nidq') # Trace of Microphone
+win.add_view(view2)
+
+view3 = ephyviewer.TraceViewer(source=sig_source3, name='signals F') # Trace of ap filtered
+win.add_view(view3)
+
+view4 = ephyviewer.SpikeTrainViewer(source=spike_source) # spiking traces
+win.add_view(view4)
 
 
-
-
-#~ view1 = ephyviewer.TraceViewer(source=sig_source1, name='signals lf')
-#~ win.add_view(view1)
-
-#~ view2 = ephyviewer.TraceViewer(source=sig_source2, name='signals nidq')
-#~ win.add_view(view2)
-
-#~ view4 = ephyviewer.TimeFreqViewer(source=sig_source1, name='timefreq')
-#~ win.add_view(view4)
-
-
-for c in range(recording_spike.get_num_channels()):
-   for view in (view0, view5):
-        if c <3:
-            view.by_channel_params[f'ch{c}', 'visible'] = True
+# Display only 2 channels
+for channel in range(recording_spike.get_num_channels()):
+   for view in (view0, view1, view3):
+        if channel <3:
+            view.by_channel_params[f'ch{channel}', 'visible'] = True
         else:
-            view.by_channel_params[f'ch{c}', 'visible'] = False
+            view.by_channel_params[f'ch{channel}', 'visible'] = False
 
 
-
-
+# Display
 win.show()
 app.exec_()
 

bee27d84a922ad632dd70a70fd414f0c6fc6dc42
SS2tower <teame4.leblois@gmail.com>
2023-04-18 11:22:50 +0200

    chunking more flexible

--- experimental_room/export_files.py
+++ experimental_room/export_files.py
@@ -5,15 +5,34 @@ import shutil
 import glob
 import os
 
-job_kwargs_local = dict(n_jobs=4 ,
+job_kwargs_local = dict(n_jobs=20,
                   chunk_duration='1s',
                   progress_bar=False,
                   )
 
-def correct_metadata(output_path_file, rec_name, rec_ap_new, rec_lf_new, rec_mic_new):
-    recs = [rec_ap_new, rec_lf_new, rec_mic_new]
-    suffixes = ['_t0.imec0.ap.meta', '_t0.imec0.lf.meta', '_t0.nidq.meta']
-    
+def correct_metadata(output_path_file, 
+                     rec_name, 
+                     rec_ap_new,
+                     rec_lf_new, 
+                     rec_mic_new):
+
+    recs = list()
+    suffixes = list()
+    if rec_ap_new is not None:
+        recs += [rec_ap_new]
+        suffixes += ['_t0.imec0.ap.meta']
+
+    if rec_mic_new is not None:
+        recs += [rec_mic_new]
+        suffixes += ['_t0.nidq.meta']
+
+    if rec_lf_new is not None:
+        recs += [rec_lf_new]
+        suffixes += ['_t0.imec0.lf.meta']
+
+
+    print(recs)
+
     for rec, suffix in zip(recs,suffixes):
         file_path = (output_path_file / (rec_name+suffix))
         meta = file_path.read_text().splitlines()
@@ -32,43 +51,56 @@ def correct_metadata(output_path_file, rec_name, rec_ap_new, rec_lf_new, rec_mic
     print('Done')  
             
 
-def export_files(rec_ap, rec_lf, rec_mic, original_path, output_path, time_range):
-    # Sampling rates
-    fs_ap = rec_ap.get_sampling_frequency()
-    fs_mic = rec_mic.get_sampling_frequency()
-    fs_lf = rec_lf.get_sampling_frequency()
-    
-    # Time range
+def export_files(rec_ap,
+                 rec_lf, 
+                 rec_mic, 
+                 original_path, 
+                 output_path, 
+                 time_range):
+     # Time range
     t0 = time_range[0]
     t1 = time_range[1]
-    
-    # Create new recs
-    rec_ap_new = rec_ap.frame_slice(start_frame=t0*fs_ap, end_frame=t1*fs_ap)
-    rec_mic_new = rec_mic.frame_slice(start_frame=t0*fs_mic, end_frame=t1*fs_mic)
-    rec_lf_new = rec_lf.frame_slice(start_frame=t0*fs_lf, end_frame=t1*fs_lf)
-    
+
     # Correct Path
     rec_name = output_path.stem
     output_path_file = Path(output_path.as_posix() + f'_from_{t0}_to_{t1}s')
-    
+
     shutil.rmtree(output_path_file, ignore_errors=True)
     output_path_file.mkdir()
-    
-    # Save new recs
-    si.write_binary_recording(rec_ap_new, file_paths=[output_path_file / (rec_name+'_t0.imec0.ap.bin')], dtype='int16', add_file_extension=False,
+
+
+    rec_ap_new = None
+    if rec_ap is not None:
+        fs_ap = rec_ap.get_sampling_frequency()
+        rec_ap_new = rec_ap.frame_slice(start_frame=t0*fs_ap, end_frame=t1*fs_ap)
+        si.write_binary_recording(rec_ap_new, file_paths=[output_path_file / (rec_name+'_t0.imec0.ap.bin')], dtype='int16', add_file_extension=False,
                            verbose=False, byte_offset=0, auto_cast_uint=False,  **job_kwargs_local)
-    si.write_binary_recording(rec_mic_new, file_paths=[output_path_file / (rec_name+'_t0.nidq.bin')], dtype='int16', add_file_extension=False,
+
+    rec_mic_new = None
+    if rec_mic is not None:
+        fs_mic = rec_mic.get_sampling_frequency()
+        rec_mic_new = rec_mic.frame_slice(start_frame=t0*fs_mic, end_frame=t1*fs_mic)
+        si.write_binary_recording(rec_mic_new, file_paths=[output_path_file / (rec_name+'_t0.nidq.bin')], dtype='int16', add_file_extension=False,
                            verbose=False, byte_offset=0, auto_cast_uint=False, **job_kwargs_local)
-    si.write_binary_recording(rec_lf_new, file_paths=[output_path_file / (rec_name+'_t0.imec0.lf.bin')], dtype='int16', add_file_extension=False,
+
+    rec_lf_new = None
+    if rec_lf is not None:
+        fs_lf = rec_lf.get_sampling_frequency()
+        rec_lf_new = rec_lf.frame_slice(start_frame=t0*fs_lf, end_frame=t1*fs_lf)
+        si.write_binary_recording(rec_lf_new, file_paths=[output_path_file / (rec_name+'_t0.imec0.lf.bin')], dtype='int16', add_file_extension=False,
                            verbose=False, byte_offset=0, auto_cast_uint=False, **job_kwargs_local)
-    
+
     # Move metadata
     files_list = glob.glob(original_path.as_posix() + '/**/**.meta', recursive=True)
     for file in files_list:
         file = Path(file)
         shutil.copy(file, output_path_file/file.parts[-1])
         
-    correct_metadata(output_path_file, rec_name, rec_ap_new, rec_lf_new, rec_mic_new)
+    correct_metadata(output_path_file, 
+                     rec_name, 
+                     rec_ap_new,
+                     rec_lf_new, 
+                     rec_mic_new)
     
     readme_path = output_path_file / 'readme.txt'
     with open(readme_path, 'w') as fp:
